dae51fbfec6c5868fa0e2c7d12d73e476c24ffb0
alan7lp <140571917+alan7lp@users.noreply.github.com>
2023-07-27 01:31:12 +0200

    Update effects.js

--- effects.js
+++ effects.js
@@ -27,3 +27,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Get the form element by its ID
+    var form = document.getElementById('contact-form');
+    // Add an event listener to the form's submit event
+    form.addEventListener('submit', function(){
+        // Reset the form after a delay to ensure the form data is sent first
+        setTimeout(function() {
+            form.reset();
+        }, 100);
+    });
+});
